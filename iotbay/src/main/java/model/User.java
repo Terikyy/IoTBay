cad6109b9ce7f94ed1efc942eab4f2c352545922
timothykugler <timmykugler@gmail.com>
2025-04-02 12:59:35 +1100

    Models and Gender removal

--- iotbay/src/main/java/model/User.java
+++ iotbay/src/main/java/model/User.java
@@ -1,121 +1,81 @@
 package model;
+
 import java.io.Serializable;
-import java.util.Objects;
 
-public class User implements Serializable{
+public /*abstract*/ class User implements Serializable {
 
-  String email;
-  String name;
-  // Password should be hashed and salted
-  String password;
-  String gender;
+    String email;
+    String name;
+    // Password should be hashed and salted (and peppered :))
+    String password;
 
-  // Address information (optional)
-  // In a real-world application, you might want to use a more complex structure for addresses
-  String adressLine="";
-  String additionalAdressInfo="";
-  Integer postalCode;
-  String StateCode="";
+    // Address information (optional)
+    // In a real-world application, you might want to use a more complex structure for addresses
+    String adressLine = "";
+    String additionalAdressInfo = "";
+    Integer postalCode;
+    String StateCode = "";
 
-  public User() {
-  }
+    // As adress information is optional, we can have a constructor without it
+    public User(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
 
-  // As adress information is optional, we can have a constructor without it
-  public User(String email, String name, String password, String gender) {
-    this.email = email;
-    this.name = name;
-    this.password = password;
-    this.gender = gender;
-  }
+    public String getEmail() {
+        return this.email;
+    }
 
-  public String getEmail() {
-    return this.email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public String getName() {
+        return this.name;
+    }
 
-  public String getName() {
-    return this.name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public String getPassword() {
+        return this.password;
+    }
 
-  public String getPassword() {
-    return this.password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public String getAdressLine() {
+        return this.adressLine;
+    }
 
-  public String getGender() {
-    return this.gender;
-  }
+    public void setAdressLine(String adressLine) {
+        this.adressLine = adressLine;
+    }
 
-  public void setGender(String gender) {
-    this.gender = gender;
-  }
+    public String getAdditionalAdressInfo() {
+        return this.additionalAdressInfo;
+    }
 
-  public String getAdressLine() {
-    return this.adressLine;
-  }
+    public void setAdditionalAdressInfo(String additionalAdressInfo) {
+        this.additionalAdressInfo = additionalAdressInfo;
+    }
 
-  public void setAdressLine(String adressLine) {
-    this.adressLine = adressLine;
-  }
+    public Integer getPostalCode() {
+        return this.postalCode;
+    }
 
-  public String getAdditionalAdressInfo() {
-    return this.additionalAdressInfo;
-  }
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
 
-  public void setAdditionalAdressInfo(String additionalAdressInfo) {
-    this.additionalAdressInfo = additionalAdressInfo;
-  }
+    public String getStateCode() {
+        return this.StateCode;
+    }
 
-  public Integer getPostalCode() {
-    return this.postalCode;
-  }
-
-  public void setPostalCode(Integer postalCode) {
-    this.postalCode = postalCode;
-  }
-
-  public String getStateCode() {
-    return this.StateCode;
-  }
-
-  public void setStateCode(String StateCode) {
-    this.StateCode = StateCode;
-  }
-  
-  @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof User)) {
-            return false;
-        }
-        User user = (User) o;
-        return Objects.equals(email, user.email) && Objects.equals(name, user.name) && Objects.equals(password, user.password) && Objects.equals(gender, user.gender) && Objects.equals(adressLine, user.adressLine) && Objects.equals(additionalAdressInfo, user.additionalAdressInfo) && Objects.equals(postalCode, user.postalCode) && Objects.equals(StateCode, user.StateCode);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(email, name, password, gender);
-  }
-
-  @Override
-  public String toString() {
-    return "{" +
-      " email='" + getEmail() + "'" +
-      ", name='" + getName() + "'" +
-      ", password='" + getPassword() + "'" +
-      ", gender='" + getGender() + "'" +
-      "}";
-  }
-  
+    public void setStateCode(String StateCode) {
+        this.StateCode = StateCode;
+    }
 }
--- iotbay/src/main/webapp/register.jsp
+++ iotbay/src/main/webapp/register.jsp
@@ -3,10 +3,9 @@
 <%
     if ("POST".equalsIgnoreCase(request.getMethod())) {
         User user = new User(
-            request.getParameter("email"),
-            request.getParameter("name"),
-            request.getParameter("password"),
-            request.getParameter("gender")
+                request.getParameter("email"),
+                request.getParameter("name"),
+                request.getParameter("password")
         );
         session.setAttribute("user", user);
         response.sendRedirect("account.jsp");
@@ -22,32 +21,28 @@
     <link rel="stylesheet" href="css/subpages/register.css">
 </head>
 <body>
-    <header>
-        <div class="logo">
-            <img src="assets/images/iotbay_logo.png" alt="IoTBay">
-        </div>
-        <a href="index.jsp">Return to Main Page</a>
-    </header>
-    <div class="container">
-        <div class="main-container">
-            <div class="centered-container">
-                <form action="register.jsp" method="post">
-                    <h2>Registration</h2>
-                    <label for="email">Email:</label><br>
-                    <input type="email" name="email" id="email" required><br>
-                    <label for="name">Name:</label><br>
-                    <input type="text" name="name" id="name" required><br>
-                    <label for="password">Password:</label><br>
-                    <input type="password" name="password" id="password" required><br>
-                    <label for="gender">Gender:</label><br>
-                    <input type="radio" name="gender" value="male" required> Male<br>
-                    <input type="radio" name="gender" value="female" required> Female<br>
-                    <input type="radio" name="gender" value="other" required> Other<br>
-                    <input type="checkbox" name="tos" id="tos" required> I agree to the Terms of Service<br>
-                    <input type="submit" value="Register">
-                </form>
-            </div>
+<header>
+    <div class="logo">
+        <img src="assets/images/iotbay_logo.png" alt="IoTBay">
+    </div>
+    <a href="index.jsp">Return to Main Page</a>
+</header>
+<div class="container">
+    <div class="main-container">
+        <div class="centered-container">
+            <form action="register.jsp" method="post">
+                <h2>Registration</h2>
+                <label for="email">Email:</label><br>
+                <input type="email" name="email" id="email" required><br>
+                <label for="name">Name:</label><br>
+                <input type="text" name="name" id="name" required><br>
+                <label for="password">Password:</label><br>
+                <input type="password" name="password" id="password" required><br>
+                <input type="checkbox" name="tos" id="tos" required> I agree to the Terms of Service<br>
+                <input type="submit" value="Register">
+            </form>
         </div>
     </div>
+</div>
 </body>
 </html>
