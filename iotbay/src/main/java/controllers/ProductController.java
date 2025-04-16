8c6c234a2014bdcbda472a42e3899741472de3d3
Terik <75367798+Terikyy@users.noreply.github.com>
2025-04-16 18:45:53 +1000

    renamed inventory controller to product controller for easier understanding

--- iotbay/src/main/java/controllers/ProductController.java
+++ iotbay/src/main/java/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package controllers;
 
-public class InventoryController {
+public class ProductController {
     // This class is responsible for managing the inventory of products in the application.
     // It handles operations such as adding, updating, and removing products from the inventory.
 }
+ 
