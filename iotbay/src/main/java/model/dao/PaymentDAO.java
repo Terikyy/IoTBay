aa3ef71a0f88068cfb9913473ed9a0eade5fa8c8
Braeden <braeden.correa@student.uts.edu.au>
2025-05-07 10:43:12 +1000

    payment note

--- iotbay/src/main/java/model/dao/PaymentDAO.java
+++ iotbay/src/main/java/model/dao/PaymentDAO.java
@@ -28,25 +28,30 @@ public class PaymentDAO extends AbstractDAO<Payment> {
     @Override
     public int insert(Payment payment) throws SQLException {
         throw new UnsupportedOperationException("Insert operation is not implemented yet.");
+        //add a new payment record to the databse. invole an INSERT sql statement
     }
 
     @Override
     public int update(Payment payment) throws SQLException {
         throw new UnsupportedOperationException("Update operation is not implemented yet.");
+        //updates an existing payment record in the database. invole an UPDATE sql statement using PaymentId as the identifier
     }
 
     @Override
     public List<Payment> get() throws SQLException {
         throw new UnsupportedOperationException("Get operation is not implemented yet.");
+        //retrieves all payment records from the databse. would involve a SELECT * FROM Payment sql query
     }
 
     @Override
     public Payment getById(int id) throws SQLException {
         throw new UnsupportedOperationException("Get operation is not implemented yet.");
+        //retrieves a single Payment record by its PaymentID. would invole a SELECT * FROM Payment WHERE PaymentID = ? sql query
     }
 
     @Override
     public int delete(int id) throws SQLException {
         throw new UnsupportedOperationException("Delete operation is not implemented yet.");
+        //deletes a payment record by its PaymentID. involves a DELETE FROM Payment WHERE PaymentID = ? sql query
     }
 }
