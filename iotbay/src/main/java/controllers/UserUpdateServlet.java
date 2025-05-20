4438425cea8ed8e2a857dfb3330711c369b854f5
timothykugler <timmykugler@gmail.com>
2025-05-20 17:24:59 +1000

    tests for log done :)

--- iotbay/src/main/java/controllers/UserUpdateServlet.java
+++ iotbay/src/main/java/controllers/UserUpdateServlet.java
@@ -6,8 +6,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import model.dao.UserDAO;
 import model.users.Customer;
+import model.users.Staff;
 import model.users.User;
 
 import java.io.IOException;
@@ -17,11 +17,6 @@ import java.sql.SQLException;
 public class UserUpdateServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        UserDAO userDAO = (UserDAO) session.getAttribute("userDAO");
-        if (userDAO == null) {
-            ConnServlet.updateDAOsPOST(request, response);
-            return;
-        }
 
         int userId = Integer.parseInt(request.getParameter("userId"));
         String name = request.getParameter("name");
@@ -29,26 +24,16 @@ public class UserUpdateServlet extends HttpServlet {
         String role = request.getParameter("role");
         String password = request.getParameter("password");
         boolean active = Boolean.parseBoolean(request.getParameter("active"));
-        System.out.println("Active: " + request.getParameter("active"));
         String addressIDString = request.getParameter("addressID");
         Integer addressID = addressIDString == null ? null : Integer.parseInt(addressIDString);
 
         User user = new Customer(userId, name, email, password, active, addressID);
+        if (role.equalsIgnoreCase("staff")) {
+            user = new Staff(user);
+        }
 
         try {
-            User oldUser = userDAO.findById(userId);
-            userDAO.update(user);
-            if (oldUser.getEmail().equals(user.getEmail()))
-                LogController.createLog(request, response, "Admin updated user " + oldUser.getEmail());
-            else
-                LogController.createLog(request, response, "Admin updated user " + oldUser.getEmail() + ". New email: " + user.getEmail());
-            if (role.equalsIgnoreCase("staff")) {
-                user.setStaff(request, response);
-                LogController.createLog(request, response, "Admin set role of " + user.getEmail() + " to staff");
-            } else {
-                user.setCustomer(request, response);
-                LogController.createLog(request, response, "Admin set role of " + user.getEmail() + " to staff");
-            }
+            UserController.updateUser(user, request, response);
         } catch (SQLException e) {
             if (e.getMessage().contains("UNIQUE constraint failed: User.Email")) {
                 session.setAttribute("update-error", "Email already exists");
